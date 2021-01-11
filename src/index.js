354687f9199b2e2d6e0316f3c22bd0252af6212b
DmitryHoncharenko <67203474+DmitryHoncharenko@users.noreply.github.com>
2021-01-11 19:42:40 +0200

    fixCode

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import './js/task-1';
-import './js/task-2';
-import './js/task-3';
+// import './js/task-2';
+// import './js/task-3';
 
 
 // const getMessage = function (callback) {
--- src/js/task-1.js
+++ src/js/task-1.js
@@ -1,14 +1,9 @@
 const delay = ms => {
-    // Твой код
-    // const time = logger(time);
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (logger(ms)) {
-                resolve;
-            }
-        }, ms);
-    })
-    
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
@@ -17,16 +12,3 @@ const logger = time => console.log(`Resolved after ${time}ms`);
 delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
-
-// const getMessage = function () {
-//   const input = prompt('Введите сообщение');
-
-//   return Promise.resolve(input);
-// };
-
-// const logger = message => console.log(message);
-
-// getMessage().then(message => logger(message));
-
-// // Или еще короче
-// getMessage().then(logger);
